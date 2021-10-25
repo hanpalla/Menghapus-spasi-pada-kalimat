fe9ef6da44d07b309848960136ef7ddcc9fc8379
Han <76608239+hanpalla@users.noreply.github.com>
2021-10-25 11:44:20 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    char kalimat[50], tanpaSpasi[25], spasi[25];
+    int i = 0, j = 0, k = 0;
+    system("cls");
+    printf("Input kalimat sebelum spasi dihapus\t: ");
+    gets(kalimat);
+    while(kalimat[i] != '\0')
+    {
+        if(kalimat[i] == ' ')
+        {
+            kalimat[i] = '\0';
+        }
+        else if (kalimat[i] != ' ')
+        {
+            tanpaSpasi[j] = kalimat[i];
+            j++;
+        }
+        i++;
+    }
+    printf("Kalimat setelah spasi terhapus\t\t: %s", tanpaSpasi);
+    return 0;
+}
