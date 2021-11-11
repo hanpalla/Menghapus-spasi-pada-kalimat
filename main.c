583ff6ed69c799e8e16a5571e254d878b6dbffc2
Han <76608239+hanpalla@users.noreply.github.com>
2021-11-11 12:02:35 +0800

    Update main.c
    Merapikan sintaks

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main()
-{
+int main(){
     char kalimat[50], tanpaSpasi[25], spasi[25];
     int i = 0, j = 0, k = 0;
     system("cls");
     printf("Input kalimat sebelum spasi dihapus\t: ");
     gets(kalimat);
-    while(kalimat[i] != '\0')
-    {
+    while(kalimat[i] != '\0'){
         if(kalimat[i] == ' ')
-        {
             kalimat[i] = '\0';
-        }
-        else if (kalimat[i] != ' ')
-        {
+        else if (kalimat[i] != ' '){
             tanpaSpasi[j] = kalimat[i];
             j++;
         }
